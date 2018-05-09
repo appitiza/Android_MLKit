dee740031545d4ca76a20e4ccd2d8d06f0aebb10
appitiza <appitiza@gmail.com>
2018-05-09 16:53:28 +0530

    test

---
+++ app/src/main/java/net/appitiza/android_mlkit/TextRecognitionActivity.java
@@ -0,0 +1,88 @@
+package net.appitiza.android_mlkit;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.text.FirebaseVisionText;
+import com.google.firebase.ml.vision.text.FirebaseVisionTextDetector;
+
+import java.util.List;
+
+public class TextRecognitionActivity extends AppCompatActivity {
+
+    private TextView mTvSample;
+    private ImageView mIvSample;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_recognition);
+
+        mTvSample = findViewById(R.id.tv_sample);
+        mIvSample = findViewById(R.id.iv_sample);
+        Bitmap image_bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.sample);
+
+        textRecognition(image_bitmap);
+
+
+    }
+
+    private void textRecognition(Bitmap bitmap) {
+        FirebaseApp.initializeApp(this);
+        FirebaseVisionImage image = FirebaseVisionImage.fromBitmap(bitmap);
+        FirebaseVisionTextDetector detector = FirebaseVision.getInstance()
+                .getVisionTextDetector();
+        detector.detectInImage(image)
+                .addOnSuccessListener(
+                        new OnSuccessListener<FirebaseVisionText>() {
+                            @Override
+                            public void onSuccess(FirebaseVisionText texts) {
+                                processResult(texts);
+                            }
+                        })
+                .addOnFailureListener(
+                        new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                // Task failed with an exception
+                                e.printStackTrace();
+                            }
+                        });
+    }
+
+
+
+    private void processResult(FirebaseVisionText texts) {
+        List<FirebaseVisionText.Block> blocks = texts.getBlocks();
+        if (blocks.size() == 0) {
+            Toast.makeText(getApplicationContext(),"No text found",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        String text = "";
+        for (int i = 0; i < blocks.size(); i++) {
+            List<FirebaseVisionText.Line> lines = blocks.get(i).getLines();
+            for (int j = 0; j < lines.size(); j++) {
+                List<FirebaseVisionText.Element> elements = lines.get(j).getElements();
+                for (int k = 0; k < elements.size(); k++) {
+                    text = text + elements.get(k).getText();
+                }
+            }
+
+        }
+        mTvSample.setText(text);
+    }
+}
