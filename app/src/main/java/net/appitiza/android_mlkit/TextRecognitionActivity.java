c7efd652c5e2028887fe8241d2c02943e69ae42d
appitiza <appitiza@gmail.com>
2018-05-09 17:04:44 +0530

    code clean

--- app/src/main/java/net/appitiza/android_mlkit/TextRecognitionActivity.java
+++ app/src/main/java/net/appitiza/android_mlkit/TextRecognitionActivity.java
@@ -5,7 +5,6 @@ import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
